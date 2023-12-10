65c3e48eb4de770c1dd1a174b8fc6961b9c39114
Your NameCamusSisyphus <masonwu624@gmail.com>
2023-12-09 21:46:51 -0500

    login /w checking user role

--- src/project/users/signin.js
+++ src/project/users/signin.js
@@ -2,18 +2,38 @@ import * as client from "./client";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 function Signin() {
-  const [credentials, setCredentials] = useState({ username: "", password: "" });
+  const [error, setError] = useState("");
+  const [user, setUser] = useState({ username: "", password: "" });
   const navigate = useNavigate();
   const signin = async () => {
-    await client.signin(credentials);
-    navigate("/project/account");
+    try {
+      await client.signin(user);
+      navigate("/project/account");
+    } catch (err) {
+      setError(err.response.data.message);
+    }
   };
+
   return (
-    <div>
+    <div className="w-50">
       <h1>Signin</h1>
-      <input className="me-2" value={credentials.username} onChange={(e) => setCredentials({...credentials, username: e.target.value})}/>
-      <input  className="me-2" type="password" value={credentials.password} onChange={(e) => setCredentials({...credentials, password: e.target.value})}/>
-      <button className="btn btn-primary me-2" onClick={signin}> Signin </button>
+      {error && <div className="alert alert-danger">{error}</div>}
+      <input
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+        className="form-control mb-2"
+      />
+      <input
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        type="password"
+        placeholder="password"
+        className="form-control mb-2"
+      />
+      <button onClick={signin} className="btn btn-primary w-100">
+        Signin
+      </button>
     </div>
   );
 }
