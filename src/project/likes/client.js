415b5c00cfe8e090ed2cf3559ecbda3e7c5deed7
Your NameCamusSisyphus <masonwu624@gmail.com>
2023-12-12 00:05:14 -0500

    add axios with credentials in like/client

--- src/project/likes/client.js
+++ src/project/likes/client.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-
+const request = axios.create({
+  withCredentials: true,
+});
 const API_BASE = process.env.REACT_APP_BASE_API_URL;
 
 const USERS_API = `${API_BASE}/api/users`;
@@ -7,22 +9,22 @@ const LIKES_API = `${API_BASE}/api/likes`;
 
 export const findAllLikes = async () => {};
 export const createUserLikesMovie = async (userId, imdbID) => {
-  const response = await axios.post(`${USERS_API}/${userId}/likes/${imdbID}`);
+  const response = await request.post(`${USERS_API}/${userId}/likes/${imdbID}`);
   return response.data;
 };
 export const deleteUserLikesMovie = async (userId, imdbID) => {
-  const response = await axios.delete(`${USERS_API}/${userId}/likes/${imdbID}`);
+  const response = await request.delete(`${USERS_API}/${userId}/likes/${imdbID}`);
   return response.data;
 };
 export const findUsersThatLikeMovie = async (imdbID) => {
-  const response = await axios.get(`${LIKES_API}/${imdbID}/users`);
+  const response = await request.get(`${LIKES_API}/${imdbID}/users`);
   return response.data;
 };
 export const findMoviesThatUserLikes = async (userId) => {
-  const response = await axios.get(`${USERS_API}/${userId}/likes`);
+  const response = await request.get(`${USERS_API}/${userId}/likes`);
   return response.data;
 };
 export const hasUserLikedMovie = async (userId, imdbID) => {
-  const response = await axios.get(`${USERS_API}/${userId}/likes/${imdbID}`);
+  const response = await request.get(`${USERS_API}/${userId}/likes/${imdbID}`);
   return response.data;
 };
